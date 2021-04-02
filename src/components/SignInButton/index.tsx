a8a2536e8896881a5502b65ce236317373b163ca
wendellcalixtoo <wendellcalixtoo@gmail.com>
2021-04-01 21:56:05 -0300

    Feat: Add SignButtonGitHub and styles :sparkles:

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,3 +1,21 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
+import styles from "./styles.module.scss";
+
 export function SignInButton() {
-  return <button type="button">Sign in with GitHub</button>;
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d461" />
+      Wendell Calixto
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign in with GitHub
+    </button>
+  );
 }
